cd439cdc09a2a596c62b534c9ed363f750869a0d
jayelg <john@raur.net>
2024-04-04 16:32:09 +1100

    Add custom Payload CMS boilerplate from johngrantdev/personal-site

---
+++ apps/backend/cms/README.md
@@ -0,0 +1,21 @@
+# Tasktime CMS
+
+## Description
+
+A CMS built using PayloadCMS and used for public pages on the frontend.
+
+## Status
+
+Initial custom configuration derived from [johngrantdev/personal-site](https://github.com/johngrantdev/personal-site).
+
+Not yet implemented with frontend.
+
+## Running the app
+
+```bash
+# development
+$ pnpm run dev
+
+# production mode
+$ pnpm run serve
+```
---
+++ apps/backend/cms/src/components/ThumbnailCell.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import type { Props } from "payload/components/views/Cell";
+
+const ThumbnailCell: React.FC<Props> = (props) => {
+  const { cellData = "" } = props;
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    if (cellData) {
+      const apiUrl = `${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/payload/uploads/${cellData}`;
+
+      fetch(apiUrl, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUrl(data.url);
+        })
+        .catch();
+    }
+  }, [cellData]);
+
+  return url ? (
+    <img src={url} style={{ maxWidth: "24em", height: "auto" }} />
+  ) : (
+    <>No image available</>
+  );
+};
+
+export default ThumbnailCell;
