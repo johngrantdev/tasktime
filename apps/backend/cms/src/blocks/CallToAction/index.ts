520e7e061517ad66ad73c7c77ac47bb86f02de60
jayelg <john@raur.net>
2024-04-04 16:32:09 +1100

    Add custom Payload CMS boilerplate from johngrantdev/personal-site

---
+++ apps/backend/cms/src/blocks/CallToAction/index.ts
@@ -0,0 +1,27 @@
+import type { Block } from "payload/types";
+
+import { invertBackground } from "../../fields/invertBackground";
+import linkGroup from "../../fields/linkGroup";
+
+export const CallToAction: Block = {
+  slug: "cta",
+  interfaceName: "CallToActionBlock",
+  labels: {
+    singular: "Call to Action",
+    plural: "Calls to Action",
+  },
+  fields: [
+    invertBackground,
+    {
+      name: "richText",
+      label: "Caption",
+      type: "richText",
+    },
+    linkGroup({
+      appearances: ["primary", "secondary"],
+      overrides: {
+        maxRows: 2,
+      },
+    }),
+  ],
+};
